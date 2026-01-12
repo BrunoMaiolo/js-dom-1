881a6ea145bf27228ce648cbca8e03392e5882e3
BrunoMaiolo <brunosj@hotmail.it>
2026-01-12 15:06:16 +0100

    spengo la lampadina

--- bonus.js
+++ bonus.js
@@ -5,6 +5,9 @@ toggleBtn.addEventListener("click", function(){
     if (lampadina.scroll.includes("white_lamp")){
         lampadina.src = "img/img/yellow_lamp.png";
         toggleBtn.textContent = "Spegni";
+    } else {
+        lampadina.src = "img/img/white_lamp.png";
+        toggleBtn.textContent = "Accendi";
     }
 
 })
