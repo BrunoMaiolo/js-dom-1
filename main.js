37388c92a2938d9dbe13897dfd3b98218e721406
BrunoMaiolo <brunosj@hotmail.it>
2026-01-12 14:35:49 +0100

    aggiungo funzione : cliccando il bottone appare immagine della lampadina accesa

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 const lampadina = document.getElementById('lampadina');
 const bottone = document.getElementById('toggleBtn');
+
+bottone.addEventListener('click', function() {
+    lampadina.src = 'img/img/yellow_lamp.png';
+
+});
