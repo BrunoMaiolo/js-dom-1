8c2e42ccaedd4fc9e5e8ab02b05b0b0703878ddc
BrunoMaiolo <brunosj@hotmail.it>
2026-01-12 15:03:58 +0100

    accendo lampadina

--- bonus.js
+++ bonus.js
@@ -3,7 +3,8 @@ const bottone = document.getElementById('toggleBtn');
 
 toggleBtn.addEventListener("click", function(){
     if (lampadina.scroll.includes("white_lamp")){
-        
+        lampadina.src = "img/img/yellow_lamp.png";
+        toggleBtn.textContent = "Spegni";
     }
 
 })
