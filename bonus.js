91c880b5deb03959d338f7a854d74889048343ef
BrunoMaiolo <brunosj@hotmail.it>
2026-01-12 14:58:34 +0100

    controllare se la lampadina e' spenta

--- bonus.js
+++ bonus.js
@@ -2,5 +2,8 @@ const lampadina = document.getElementById('lampadina');
 const bottone = document.getElementById('toggleBtn');
 
 toggleBtn.addEventListener("click", function(){
+    if (lampadina.scroll.includes("white_lamp")){
+        
+    }
 
 })
